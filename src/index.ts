e47de1279ebfbbdf5ba78967905e59ddc20c0f24
DiogoJorge1401 <diogojorge1401@gmail.com>
2025-01-24 10:26:30 -0300

    feat(scraper): add cron job to scrape GitHub trending repositories daily

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import { cron } from '@elysiajs/cron';
+import { scrapeTrendingRepos } from './data-collect/scrape-trending-repos';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    cron({
+      name: 'scraping',
+      pattern: '0 0 * * *',
+      run() {
+        console.log('Running cron job to scrape GitHub trending repositories');
+        return scrapeTrendingRepos();
+      }
+    })
+  )
+  .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
